9589016f870e9be925b101b975dae3f1e16d0098
Madhu Pal <madhupal888@gmail.com>
2025-04-16 17:25:49 +0530

    Update Combined.sql

--- Combined.sql
+++ Combined.sql
@@ -2,22 +2,32 @@ Select * from Books;
 Select * from Orders;
 Select * from Customers;
 
-1. Select * from Books where Genre='Fiction';
+--1) Retrieve all books in the "Fiction" genre:
+Select * from Books
+where Genre='Fiction';
 
-2. Select * from Books where Published_Year>1950;
+-- 2) Find books published after the year 1950:
+Select * from Books
+where Published_Year>1950;
 
-3. Select * from Customers where country = 'Canada';
+-- 3) List all customers from the Canada:
+Select * from Customers
+where country = 'Canada';
 
-4. Select * from Orders where Order_date >= '2023-11-01' and Order_date < '2023-12-01';
+-- 4) Show orders placed in November 2023:
+Select * from Orders 
+where Order_date >= '2023-11-01'
+and Order_date < '2023-12-01';
 
-5.Select sum(stock) from Books;
+-- 5) Retrieve the total stock of books available:
+Select sum(stock) as Total_Stock from Books;
 
-6.Select MAX(price) from Books;
-or 
-select  top 1 * from Books
+-- 6) Find the details of the most expensive book:
+Select  top 1 * from Books
 order by Price desc;
 
-7. Select c.name, c.email, o.order_date from 
+-- 7) Show all customers who ordered more than 1 quantity of a book:
+Select c.name, c.email, o.order_date from 
 Customers as c join
 Orders as o on
 c.Customer_id = o.customer_id
@@ -26,20 +36,25 @@ where o.quantity >1;
 Select * from Orders
 where Quantity>1;
 
-8. Select * from Orders where Total_Amount>20;
+-- 8) Retrieve all orders where the total amount exceeds $20:
+Select * from Orders 
+where Total_Amount>20;
 
-9. Select distinct(Genre) from Books;
+-- 9) List all genres available in the Books table:
+Select distinct(Genre) from Books;
 
-10.Select min(stock) from Books;
+-- 10) Find the book with the lowest stock:
+Select min(stock) from Books;
 or
 Select Top 1 * from Books order by Stock Asc;
 
-
-11. Retrive the total books 
+-- 11) Calculate the total revenue generated from all orders:
 Select sum( Total_Amount) as Total_Revenue from Orders;
 
+-- Advance Questions : 
 
-12. Select b.Genre, Sum(o.Quantity) as total_book_Sold from 
+-- 1) Retrieve the total number of books sold for each genre:
+ Select b.Genre, Sum(o.Quantity) as total_book_Sold from 
 Books as b
 join
 Orders as o
@@ -47,19 +62,11 @@ on
 b.Book_ID= o.Book_ID
 Group by b.Genre;
 
-13. Select AVG(Price) from Books where Genre='Fantasy';
+-- 2) Find the average price of books in the "Fantasy" genre:
+Select AVG(Price) from Books
+where Genre='Fantasy';
 
-14.
-Select o.Customer_ID,c.name, COUNT(o.order_Id) from
-Orders as o
-join
-Customers as c
-on 
-o.Customer_ID=c.Customer_ID
-group by o.Customer_ID
-having Count(o.Order_ID)>= 2;
-
-15.
+-- 3) List customers who have placed at least 2 orders:
 Select o.Customer_ID,c.Name ,COUNT(o.order_ID) as Order_Count From 
 Orders as o 
 join
@@ -69,8 +76,8 @@ o.Customer_ID = c.Customer_ID
 Group by o.Customer_ID,c.Name
 Having COUNT(o.Order_ID)>=2;
 
-
-16.Select top 1 o.Book_ID,b.Title,count(o.Order_ID) as Order_Count 
+-- 4) Find the most frequently ordered book:
+Select top 1 o.Book_ID,b.Title,count(o.Order_ID) as Order_Count 
 from 
 Orders as o join
 Books as b on
@@ -79,19 +86,22 @@ group by o.Book_ID,b.Title
 Order by Order_Count desc;
 
 
-17. Select top 3 * from Books
+-- 5) Show the top 3 most expensive books of 'Fantasy' Genre :
+Select top 3 * from Books
 where Genre= 'Fantasy'
 order by price desc;
 
 
-18. Select b.Author, Sum(o.Quantity) as Total_Quantity from Books as b
+-- 6) Retrieve the total quantity of books sold by each author:
+Select b.Author, Sum(o.Quantity) as Total_Quantity from Books as b
 join
 Orders as o
 on
 b.Book_ID= o.Book_ID
 group by b.Author;
 
-19. Select Distinct(c.city), o.Total_Amount 
+-- 7) List the cities where customers who spent over $30 are located:
+Select Distinct(c.city), o.Total_Amount 
 from 
 Customers as c
 join
@@ -100,7 +110,15 @@ on
 c.Customer_ID= o.Customer_ID
 where o.Total_Amount>30;
 
-20. Select b.Book_ID, b.Stock, Coalesce(sum(Quantity),0) as Quantity_Use, (b.Stock-Coalesce(sum(Quantity),0)) as Remaining_Quantity
+-- 8) Find the customer who spent the most on orders:
+SELECT Top 1 c.customer_id, c.name, SUM(o.total_amount) AS Total_Spent
+FROM orders o
+JOIN customers c ON o.customer_id=c.customer_id
+GROUP BY c.customer_id, c.name
+ORDER BY Total_spent Desc;
+
+--9) Calculate the stock remaining after fulfilling all orders:
+Select b.Book_ID, b.Stock, Coalesce(sum(Quantity),0) as Quantity_Use, (b.Stock-Coalesce(sum(Quantity),0)) as Remaining_Quantity
 from Books as b
 Left join 
 Orders as o on
